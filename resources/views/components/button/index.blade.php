188402eaf7f877d4201cb3f78c1d559a42d7a19a
David Gutierrez <david@okeano.com>
2026-03-25 01:46:56 +0100

    Add as prop to button component for polymorphic rendering

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,8 +1,9 @@
 {{-- 
-@description Button. Set variant="primary|danger|ghost|..." and size="sm|lg|...". Add icon="name" for leading icon, icon:trailing="name" for trailing. Use square for icon-only buttons. The primary variant is used automatically when no variant prop is passed, so variant="primary" is not needed.
+@description Button. Set variant="primary|danger|ghost|..." and size="sm|lg|...". Add icon="name" for leading icon, icon:trailing="name" for trailing. Use square for icon-only buttons. Use as="a" with href="..." to render as a link. The primary variant is used automatically when no variant prop is passed, so variant="primary" is not needed.
 @variants primary, secondary, outline, ghost, subtle, danger, danger-soft
 @sizes xs, sm, md, lg, xl
 @usage <x-boson::button icon="check">Save</x-boson::button>
+@usage <x-boson::button as="a" href="/home">Home</x-boson::button>
 --}}
 
 @php
@@ -11,17 +12,18 @@
     $iconAttrs = Boson::extract($attributes, 'icon');
     $btnAttrs = Boson::except($attributes, 'icon');
 
+    $as = $btnAttrs->get('as', 'button');
     $icon = $btnAttrs->get('icon');
     $iconTrailing = $iconAttrs->get('trailing');
 
-    $el = Boson::element('button')
+    $el = Boson::element($as)
         ->base('btn')
         ->mod($btnAttrs->get('variant', 'primary'))
         ->mod($btnAttrs->get('size', 'md'))
         ->when($btnAttrs->get('square'), 'mod', 'square');
 @endphp
 
-<{{ $el->getElement() }} {{ $btnAttrs->except(['icon', 'variant', 'size', 'square'])->merge($el->getMergeAttributes()) }}>
+<{{ $el->getElement() }} {{ $btnAttrs->except(['icon', 'variant', 'size', 'square', 'as'])->merge($el->getMergeAttributes()) }}>
     @if ($icon)
         <x-boson::icon :name="$icon" />
     @endif
